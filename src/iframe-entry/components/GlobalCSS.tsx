28fbcd3c0f28cdfd66d41f1a6dba78ef938abf8b
finico <agonchar@web3tech.ru>
2021-01-25 03:29:23 +0300

    DEXW-4007: Use object notation for styles and make symlink for fonts

--- src/iframe-entry/components/GlobalCSS.tsx
+++ src/iframe-entry/components/GlobalCSS.tsx
@@ -1,93 +1,101 @@
 import React, { FC } from 'react';
-import { Global, css } from '@emotion/core';
+import { Global } from '@emotion/core';
 
 export const GlobalCSS: FC = () => {
     return (
         <Global
-            styles={css`
-                @font-face {
-                    font-family: 'Roboto';
-                    font-style: normal;
-                    font-weight: 300;
-                    font-display: swap;
-                    src: url(./fonts/roboto-light7.woff2) format('woff2');
-                }
-                @font-face {
-                    font-family: 'Roboto';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url('./fonts/roboto7.woff2') format('woff2');
-                }
-                @font-face {
-                    font-family: 'Roboto';
-                    font-style: normal;
-                    font-weight: 700;
-                    font-display: swap;
-                    src: url('./fonts/roboto-medium7.woff2') format('woff2');
-                }
-                html {
-                    height: 100%;
-                }
-                body {
-                    font-family: 'Roboto', sans-serif;
-                    margin: 0;
-                    padding: 0;
-                    min-height: 100vh;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    -webkit-overflow-scrolling: touch;
-                }
-                @supports (-webkit-touch-callout: none) {
-                    body {
-                        min-height: -webkit-fill-available;
-                    }
-                }
-                button {
-                    font-family: 'Roboto', sans-serif;
-                }
-                #root {
-                    position: relative;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    width: 520px;
-                    min-height: 100%;
-                }
-                #overlay {
-                    position: fixed;
-                    z-index: -1;
-                    top: 0;
-                    left: 0;
-                    height: 100%;
-                    width: 100%;
-                    overflow: hidden;
-                    background-color: #000;
-                    opacity: 0.6;
-                }
-                * {
-                    box-sizing: border-box;
-                }
-                ::-webkit-scrollbar-thumb {
-                    background-color: #495060;
-                    border-radius: 4px;
-                }
-                ::-webkit-scrollbar {
-                    width: 3px;
-                    height: 3px;
-                }
-                ::-webkit-scrollbar-corner {
-                    opacity: 0;
-                }
-                ::-webkit-scrollbar-track-piece {
-                    margin-bottom: 5px;
-                    margin-top: 5px;
-                }
-                ::placeholder {
-                    color: #959dae;
-                }
-            `}
+            styles={[
+                {
+                    '@font-face': {
+                        fontFamily: 'Roboto',
+                        fontStyle: 'normal',
+                        fontWeight: 300,
+                        fontDisplay: 'swap',
+                        src: 'url(/fonts/roboto-light7.woff2) format("woff2")',
+                    },
+                },
+                {
+                    '@font-face': {
+                        fontFamily: 'Roboto',
+                        fontStyle: 'normal',
+                        fontWeight: 400,
+                        fontDisplay: 'swap',
+                        src: 'url(/fonts/roboto7.woff2) format("woff2")',
+                    },
+                },
+                {
+                    '@font-face': {
+                        fontFamily: 'Roboto',
+                        fontStyle: 'normal',
+                        fontWeight: 700,
+                        fontDisplay: 'swap',
+                        src: 'url(/fonts/roboto-medium7.woff2) format("woff2")',
+                    },
+                },
+                {
+                    '*': {
+                        boxSizing: 'border-box',
+                    },
+                    html: {
+                        height: '100%',
+                    },
+                    body: {
+                        fontFamily: 'Roboto, sans-serif',
+                        margin: 0,
+                        padding: 0,
+                        minHeight: '100vh',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        WebkitOverflowScrolling: 'touch',
+                    },
+                    '@supports (-webkit-touch-callout: none)': {
+                        body: {
+                            minHeight: '-webkit-fill-available',
+                        },
+                    },
+                    button: {
+                        fontFamily: 'Roboto, sans-serif',
+                    },
+                    '#root': {
+                        position: 'relative',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '520px',
+                        minHeight: '100%',
+                    },
+                    '#overlay': {
+                        position: 'fixed',
+                        zIndex: -1,
+                        top: 0,
+                        left: 0,
+                        height: '100%',
+                        width: '100%',
+                        overflow: 'hidden',
+                        backgroundColor: '#000',
+                        opacity: 0.6,
+                    },
+                    '::-webkit-scrollbar-thumb': {
+                        backgroundColor: '#495060',
+                        borderRadius: '4px',
+                    },
+                    '::-webkit-scrollbar': {
+                        width: '3px',
+                        height: '3px',
+                    },
+                    '::-webkit-scrollbar-corner': {
+                        opacity: 0,
+                    },
+                    '::-webkit-scrollbar-track-piece': {
+                        marginBottom: '5px',
+                        marginTop: '5px',
+                    },
+                    '::placeholder': {
+                        color: '#959dae',
+                    },
+                },
+            ]}
         />
     );
 };
