e7c714bc6e203b5729f8af7e77404713cf0858aa
Max Axenov <maksenov@wavesplatform.com>
2021-06-11 14:36:16 +0300

    fix: remove recaptcha

--- packages/provider-cloud-ui/src/components/Auth/SignUpForm.tsx
+++ packages/provider-cloud-ui/src/components/Auth/SignUpForm.tsx
@@ -357,43 +357,6 @@ export const SignUpForm: FC<SignUpFormProps> = ({ signUp, onSignInClick }) => {
                     Log In
                 </Button>
             </Text>
-            <Flex
-                mt={24}
-                fontSize="12px"
-                lineHeight="14px"
-                color="basic.$700"
-                textAlign="left"
-                display="inline-block"
-                py="4px"
-                justifyContent="center"
-            >
-                <Flex justifyContent="center">
-                    <Text textAlign="center">
-                        This site is protected by reCAPTCHA
-                    </Text>
-                </Flex>
-                <Flex justifyContent="center">
-                    <Text mr={4}>and the Google</Text>
-                    <ExternalLink
-                        href="https://policies.google.com/privacy"
-                        target="_blank"
-                        referrerPolicy="noopener noreferrer"
-                    >
-                        <Text color="basic.$500">Privacy Policy</Text>
-                    </ExternalLink>
-                    <Text mr={4} ml={4}>
-                        and
-                    </Text>
-                    <ExternalLink
-                        href="https://policies.google.com/terms"
-                        target="_blank"
-                        referrerPolicy="noopener noreferrer"
-                    >
-                        <Text color="basic.$500">Terms of Service apply</Text>
-                    </ExternalLink>
-                    <Text>.</Text>
-                </Flex>
-            </Flex>
         </Flex>
     );
 };
